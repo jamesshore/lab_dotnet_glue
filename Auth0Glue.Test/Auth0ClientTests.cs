47de1a6f2c7c5e9f05f7d22b8d5ab0ea9c80cf89
James Shore <jshore@jamesshore.com>
2017-12-01 14:30:31 -0800

    Stub in Auth0Client class and tests

---
+++ Auth0Glue.Test/Auth0ClientTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Auth0Glue.Test
+{
+    [TestClass]
+    public class Auth0ClientTests
+    {
+        string Auth0Domain = "auth0_domain";
+        string Auth0Id = "auth0_id";
+        string Auth0Secret = "auth0_secret";
+        string Auth0ApiToken = "auth0_api_token";
+        string Auth0Connection = "auth0_connection";
+
+        [TestMethod]
+        public async Task SendsPasswordResetEmailAsync()
+        {
+            var email_address = "an_email_address";
+
+            var client = new Auth0Client(Auth0Domain, Auth0Id, Auth0Secret, Auth0ApiToken, Auth0Connection);
+            await client.SendPasswordResetEmail(email_address);
+        }
+    }
+}
---
+++ Auth0Glue/Auth0Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Auth0Glue
+{
+    public class Auth0Client
+    {
+        private object auth0Domain;
+        private object auth0Id;
+        private object auth0Secret;
+        private object auth0ApiToken;
+        private object auth0Connection;
+
+        public Auth0Client(object auth0Domain, object auth0Id, object auth0Secret, object auth0ApiToken, object auth0Connection)
+        {
+            this.auth0Domain = auth0Domain;
+            this.auth0Id = auth0Id;
+            this.auth0Secret = auth0Secret;
+            this.auth0ApiToken = auth0ApiToken;
+            this.auth0Connection = auth0Connection;
+        }
+
+        public async Task SendPasswordResetEmail(string email)
+        {
+
+        }
+    }
+}
